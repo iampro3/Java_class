3e3fa9fe70e2af7023ecc7facfe5bb158a77b930
iampro3 <lucky1977@daum.net>
2023-03-10 12:53:57 +0900

    score 코드 리뷰

--- java_mission/src/java_mission/Score.java
+++ java_mission/src/java_mission/Score.java
@@ -9,13 +9,15 @@ public class Score {
 		System.out.print("영어점수 : ");		
 		int eng = sc.nextInt();
 		
-		System.out.print("영어점수 : ");		
-		int eng = sc.nextInt();
+		System.out.print("수학점수 : ");		
+		int math = sc.nextInt();
 		
-		System.out.print("영어점수 : ");		
-		int eng = sc.nextInt();
+		System.out.print("자바점수 : ");		
+		int java = sc.nextInt();
 		
-		
+		int sun = 0;
+		double avg = 0.0;
+		sum = eng+
 	}
 	
 }
